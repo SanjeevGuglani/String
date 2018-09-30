ad6bbc8df8e9b4515b52d17b7d0a3f344fcf73bf
B0204071 <sanjeev.guglani@airtelbank.com>
2018-09-30 15:46:43 +0530

    Return Permutation of String

--- src/com/sanjeev/StringPermutations.java
+++ src/com/sanjeev/StringPermutations.java
@@ -1,5 +1,8 @@
 package com.sanjeev;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Sanjeev Guglani
  */
@@ -19,8 +22,25 @@ public class StringPermutations {
             }
         }
     }
+
+    public static List<String> permutation(String str){
+        List<String> list=new ArrayList<String>();
+        if(str.equals("")){
+            list.add("");
+        }else {
+            for(int i=0;i<str.length();i++){
+                List<String> temp = permutation(str.substring(0,i)+str.substring(i+1));
+                for(int j=0;j<temp.size();j++){
+                   list.add(str.charAt(i)+temp.get(j));
+                }
+            }
+        }
+        return list;
+    }
+    
     public static void main(String[] args) {
         String str ="MEN";
         printPermutations(str,0);
+        System.out.println(permutation("MEN"));
     }
 }
